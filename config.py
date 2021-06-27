db7bc74440a7ef1abfadd7e7ee8de45ca64e3ebe
pahanharsh <82528202+pahanharsh@users.noreply.github.com>
2021-06-27 11:19:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "1845678790:AAEfL-65ifZSlK5aReTyV6ZjC633DRkjUJI" 
+    BOT_TOKEN = "1823423119:AAGXua8N-hafHfIzmeoQt3PLX-u7xpX9TvI" 
     API_ID =  5729824
     API_HASH = "6e540919758de4eefadca5ed9f182b0a"
     
